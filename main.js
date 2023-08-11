2a5abc86778ff81f91b4875d2cfd17bedaf192c9
ryoma <ryomams@users.noreply.github.com>
2023-08-11 14:35:51 -0700

    Updated renderer

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
+//import three
 import * as THREE from 'three';
 const scene = new THREE.Scene();
 console.log('this is how my dad learned inglish.');
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-
+//declarations for ease of use
+const X_AXIS = new THREE.Vector3(1, 0, 0);
+const Y_AXIS = new THREE.Vector3(0, 1, 0);
  
-// renderer
+// renderer and camera
+const camera = new THREE.PerspectiveCamera(75, 1280 / 720, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
-renderer.setClearColor(0xDEADBE, 1);
+renderer.setSize(1280, 720);
+renderer.setClearColor(0xDEADAD, 1);
 document.body.appendChild(renderer.domElement);
+
 
 renderer.render(scene, camera);
